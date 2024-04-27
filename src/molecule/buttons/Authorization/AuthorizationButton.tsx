cfacc6eb97b28e149346961c5760a38ab706b95e
yukky9 <kozhurinayulia@gmail.com>
2024-04-27 09:14:14 +0300

    components

---
+++ src/molecule/buttons/Authorization/AuthorizationButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorizationButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthorizationButton;
---
+++ src/molecule/buttons/SendMassage/SendMessageButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SendMessageButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SendMessageButton;
