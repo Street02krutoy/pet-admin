1a216d864dc0cdbe274e0259b9262d8a2ba95ec7
yukky9 <kozhurinayulia@gmail.com>
2024-04-27 09:49:50 +0300

    components

---
+++ src/atoms/textField/LogIn/idField/IdField.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IdField = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default IdField;
---
+++ src/atoms/textField/Message/MessageField.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MessageField = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default MessageField;
