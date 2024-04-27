923ac62e42daba0ff044e87b562dad7567d972f3
srite <masaanekdot@gmail.com>
2024-04-27 07:10:15 +0300

    remove bootstrap :x: add tailwind :white_check_mark:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import AppBar from './components/AppBar';
 
 function App() {
   return (
     <div className="App">
-      <Button>asd</Button>
+      <AppBar/>
+      <div className='grid grid-cols-4'>
+      <div className="col-start-1 h-screen" style={{backgroundColor: "white"}}>01</div>
+  <div className="col-start-2 col-end-4">02</div>
+      </div>
     </div>
+    
   );
 }
 
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,9 @@
+
+const AppBar = () => {
+  
+ return (
+    <></>
+    );
+};
+
+export default AppBar;
