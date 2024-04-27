7109b437fafb75ad265a418f111767641ce6f14e
yukky9 <kozhurinayulia@gmail.com>
2024-04-27 12:51:01 +0300

    components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import Header from '../components/organism/header/Header';
 import GeneralPage from '../components/pages/GeneralPage/GeneralPage';
+import AuthorizationButtonForm from '../components/atoms/buttonForm/AuthorizationButton/AuthorizationButtonForm';
 
 const App = () => {
     return (
--- src/components/atoms/buttonForm/AuthorizationButton/AuthorizationButtonForm.tsx
+++ src/components/atoms/buttonForm/AuthorizationButton/AuthorizationButtonForm.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const AuthorizationButtonForm = () => {
     return (
-        <div>
-            
-        </div>
+        <button className="w-20 bg-violet hover:bg-violet
+        text-white font-bold py-2 px-4 border border-black hover:border-transparent rounded">
+        </button>
     );
 };
 
